76c6e097a0c68ca35bb532c326f6504bed26f655
Chun-Heng Tai <chtai@google.com>
2025-01-08 12:07:50 -0800

    update

--- engine/src/flutter/shell/platform/embedder/fixtures/main.dart
+++ engine/src/flutter/shell/platform/embedder/fixtures/main.dart
@@ -306,7 +306,7 @@ Future<void> a11y_main() async {
         )
         ..updateCustomAction(id: 21, label: 'Archive', hint: 'archive message');
 
-  PlatformDispatcher.instance.views.first.setSemanticsTreeEnabled(true);
+  PlatformDispatcher.instance.setSemanticsTreeEnabled(true);
   PlatformDispatcher.instance.views.first.updateSemantics(builder.build());
 
   signalNativeTest();
@@ -393,7 +393,7 @@ Future<void> a11y_string_attributes() async {
         additionalActions: Int32List(0),
       );
 
-  PlatformDispatcher.instance.views.first.setSemanticsTreeEnabled(true);
+  PlatformDispatcher.instance.setSemanticsTreeEnabled(true);
   PlatformDispatcher.instance.views.first.updateSemantics(builder.build());
   signalNativeTest();
 }
