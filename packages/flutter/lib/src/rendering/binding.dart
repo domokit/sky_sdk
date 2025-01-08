2adadbb53442e91df201f573610bbe9e91490682
Chun-Heng Tai <chtai@google.com>
2025-01-08 09:57:44 -0800

    update

--- packages/flutter/lib/src/rendering/binding.dart
+++ packages/flutter/lib/src/rendering/binding.dart
@@ -62,6 +62,7 @@ mixin RendererBinding
     if (kIsWeb) {
       addPostFrameCallback(_handleWebFirstFrame, debugLabel: 'RendererBinding.webFirstFrame');
     }
+    addSemanticsEnabledListener(_handleSemanticsEnabledChanged);
     rootPipelineOwner.attach(_manifold);
   }
 
@@ -382,6 +383,10 @@ mixin RendererBinding
   /// This is used by the [PaintingContext] after creating a [PictureRecorder]
   /// using [createPictureRecorder].
   Canvas createCanvas(ui.PictureRecorder recorder) => Canvas(recorder);
+
+  void _handleSemanticsEnabledChanged() {
+    platformDispatcher.implicitView!.setSemanticsTreeEnabled(semanticsEnabled);
+  }
 
   /// Called when the system metrics change.
   ///
