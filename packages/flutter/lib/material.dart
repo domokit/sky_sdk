rkishan516/flutter

Refactor: Move sliders component to seperate file

packages/flutter/lib/material.dart
@@ -143,6 +143,7 @@ export 'src/material/radio.dart';
 export 'src/material/radio_list_tile.dart';
 export 'src/material/radio_theme.dart';
 export 'src/material/range_slider.dart';
+export 'src/material/range_slider_components.dart';
 export 'src/material/refresh_indicator.dart';
 export 'src/material/reorderable_list.dart';
 export 'src/material/scaffold.dart';
@@ -158,6 +159,7 @@ export 'src/material/selectable_text.dart';
 export 'src/material/selection_area.dart';
 export 'src/material/shadows.dart';
 export 'src/material/slider.dart';
+export 'src/material/slider_components.dart';
 export 'src/material/slider_theme.dart';
 export 'src/material/snack_bar.dart';
 export 'src/material/snack_bar_theme.dart';

domokit/sky_sdk

packages/flutter/lib/material.dart
@@ -143,6 +143,7 @@ export 'src/material/radio.dart';
 export 'src/material/radio_list_tile.dart';
 export 'src/material/radio_theme.dart';
 export 'src/material/range_slider.dart';
+export 'src/material/range_slider_parts.dart';
 export 'src/material/refresh_indicator.dart';
 export 'src/material/reorderable_list.dart';
 export 'src/material/scaffold.dart';
@@ -158,6 +159,7 @@ export 'src/material/selectable_text.dart';
 export 'src/material/selection_area.dart';
 export 'src/material/shadows.dart';
 export 'src/material/slider.dart';
+export 'src/material/slider_parts.dart';
 export 'src/material/slider_theme.dart';
 export 'src/material/snack_bar.dart';
 export 'src/material/snack_bar_theme.dart';